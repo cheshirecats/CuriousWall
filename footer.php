95ffe51789ccf5aed49e29aef714763203726016
cheshirecat <curiouswall@gmail.com>
2012-07-19 20:51:15 +0800

    correct jquery link

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 </div>
 </body>
-<script type="text/javascript" src="jquery.js"></script>
+<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
 <script type="text/javascript" src="script.js"></script>
 </html>
