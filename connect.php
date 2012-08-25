2d39db9549937b5b64ab89824c51084d6e4ed040
Christof <christofian@gmail.com>
2012-08-25 15:02:42 -0300

    mods and locked topics

--- connect.php
+++ connect.php
@@ -10,4 +10,20 @@ $db_name    = 'test';
 $db = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass
       , array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
 );
+
+mysql_select_db("test") or die(mysql_error());
+
+// display diamond next to admins/mods name, and output the username
+
+function theusername($name,$permissions) {
+if ($permissions == 1) echo $name . "♦";
+else echo $name;
+}
+
+
+// display if a topic is locked in the topics title
+function displaytopiclocked($topicname,$islocked) {
+if ($islocked == 1) echo $topicname . " [locked]";
+else echo $topicname;
+}
 ?>
