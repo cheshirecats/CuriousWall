239552b83f154321c46c60a42fa0f6457f897a9a
Christof <christofian@gmail.com>
2012-08-25 15:46:53 -0300

    Fixed html bug and changed stick lables

--- index.php
+++ index.php
@@ -32,10 +32,8 @@
     <div id="post_container">
     <input type="text" id="post_title" style="width:100%; margin-bottom:11px;display:none" placeholder="Title"></input>
     <textarea id="post_text"  style="margin-bottom:13px" placeholder="Text"></textarea>
-    <!--<input type="checkbox" name="sticky" id="sticky" value="Sticky"><lable for="sticky">Stick this topic</lable>&nbsp;&nbsp;&nbsp;-->
-    <!--<input type="checkbox" id="sticky" name="sticky" value="Sticky"><label for="sticky">Stick this topic</label>-->
-    <?php /* display mod tools only to mods */ if ((isset($_SESSION['permissions']) && ($_SESSION['permissions'] == 1))) { echo '<div id="stickingswitch" class="radioswitch"><input type="radio" id="nostick" class="inputsticky" name="stick" value="nostick"><label for="nostick">Not sticky</label><input type="radio" id="ystick" class="inputsticky" name="stick" value="ystick"><label for="ystick">Sticky</label>';}else {}?>
-    <?php /* locking switch */ if ((isset($_SESSION['permissions']) && ($_SESSION['permissions'] == 1))) { echo '<input type="radio" id="nolock" class="inputlocked" name="lock" value="nolock"><label for="nolock">Unlock</label><div id="lockingswitch" class="radioswitch"><input type="radio" id="ylock" class="inputlocked" name="lock" value="ylock"><label for="ylock">Lock</label></div>';}else {} /**/?>
+    <?php /* display mod tools only to mods */ if ((isset($_SESSION['permissions']) && ($_SESSION['permissions'] == 1))) { echo '<div id="stickingswitch" class="radioswitch"><input type="radio" id="nostick" class="inputsticky" name="stick" value="nostick"><label for="nostick">Unstick</label><input type="radio" id="ystick" class="inputsticky" name="stick" value="ystick"><label for="ystick">Stick</label></div>';}else {}?>
+    <?php /* locking switch */ if ((isset($_SESSION['permissions']) && ($_SESSION['permissions'] == 1))) { echo '<div id="stickingswitch" class="radioswitch"><input type="radio" id="nolock" class="inputlocked" name="lock" value="nolock"><label for="nolock">Unlock</label><div id="lockingswitch" class="radioswitch"><input type="radio" id="ylock" class="inputlocked" name="lock" value="ylock"><label for="ylock">Lock</label></div>';}else {} /**/?>
     <div class="button" id="post_button" style="display:inline"><i class="icon-flag"></i>&nbsp;Post&nbsp;</div>
     <div class="button" id="page_top_button" style="float:right;margin-top:-4px;margin-right:-36px;padding:4px 10px;"><i class="icon-chevron-up"></i></div>
     <div style="display:inline" id="post_msg"></div>
