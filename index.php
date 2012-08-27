9576b81319728dd453469e37914fdce46beb11a0
Christof <christofian@gmail.com>
2012-08-26 21:38:59 -0300

    More non-javascript support

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
-
-
-
-  require_once 'header.php';
+if ($_GET['_escaped_fragment_']){require_once 'noscript.php';die;}
+else {require_once 'header.php';}
 //die();
 ?>
 <div id="left_container">
